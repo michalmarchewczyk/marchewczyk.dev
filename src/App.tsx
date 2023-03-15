cfb9e0f90356c5a573dec95e2ccfa8f80fc77a07
Michał Marchewczyk <dev@marchewczyk.eu>
2023-03-15 13:02:32 +0100

    Setup React project

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom';
+import Layout from './routes/Layout';
+import Home from './routes/Home';
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<Home/>}/>
+        </Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
